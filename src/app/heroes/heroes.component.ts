4010bc6957a1210469b63bce5b9a9ec96fa5e37a
alexiscash <alexiscash97@rocketmail.com>
2022-01-06 02:39:22 -0600

    done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,20 +9,30 @@ import { HEROES } from './mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes: Hero[] = [];
 
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
 
-  selectedHero?: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
-  constructor() {}
+  add(name: string): void {
+    name = name.trim();
 
-  ngOnInit(): void {}
+    if (!name) return;
+
+    this.heroService
+      .addHero({ name } as Hero)
+      .subscribe((hero: Hero) => this.heroes.push(hero));
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
